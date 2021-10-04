d3588727240ee0b51fb7f7b7cefbe7863d9b824e
thiago fernandes <tjfctigo@gmail.com>
2021-10-04 17:17:32 -0300

    first version

---
+++ index.js
@@ -0,0 +1,55 @@
+import { championsList } from "./data/champions.js";
+
+const configs = {
+    //sorteia um campeao aleatorio
+    sortChamp() {
+        return championsList[Math.round(Math.random() * championsList.length)]
+    },
+
+    printResults() {
+        const champion = configs.sortChamp()
+
+        document.querySelector('.champions-data').innerHTML = `
+        <h1 class="champion-name">${champion.name}</h1>
+        <img src="${champion.image}" alt="champion image" class="champion-image">
+        `
+    },
+
+    printWithAnimation() {
+        // faz a animação de mudar o nome e o campeao 10x ao clicar em gerar campeao
+        for(let transition = 100; transition <= 1000; transition += 100) {
+            setTimeout(configs.printResults, transition)
+        }       
+    },
+
+    buttonCooldown() {       
+         // define o tempo em segundos
+        let time = 120;
+        // diminui 1 do variavel time a cada 1 segundo
+        const timeCount = setInterval(() => time-- , 1000)
+        //verifica a cada 20 segundos se o tempo acabou
+        setInterval(() => {
+            if (time <= 0) { 
+                clearInterval(timeCount)
+            } 
+        }, 120 * 1000)
+        //exibe na tela o tempo restante em segundos
+        const printResults = setInterval(() => { 
+            //seleciona o <p> e exibe os segundos
+            let timeLeft = document.querySelector('#timer')
+            timeLeft.innerText = `${time}` 
+            //quando o tempo acaba, para o timer e para de exibir os segundos
+            if(time == 0) {
+                clearInterval(printResults)
+                timeLeft.innerText = ""
+            } 
+    }, 1000)
+
+        
+        
+    }
+}
+
+export { configs }
+
+//console.log(sortChamp())
